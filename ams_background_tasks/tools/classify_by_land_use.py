763f3674e8ad9e4d99f343fb78fbd11447f4000c
marcoaosilva <mac.aurelio@gmail.com>
2025-01-07 15:27:57 -0300

    fixing when geocode is null

--- ams_background_tasks/tools/classify_by_land_use.py
+++ ams_background_tasks/tools/classify_by_land_use.py
@@ -251,7 +251,7 @@ def process_active_fires_land_structure(
     sql = f"""
         SELECT id as gid, biome, geocode, geom
         FROM fires.active_fires
-        WHERE biome='{biome}'
+        WHERE biome='{biome}' AND geocode IS NOT NULL
     """
 
     logger.debug(sql)
@@ -378,7 +378,7 @@ def process_deter_land_structure(
     sql = f"""
         SELECT gid, biome, geocode, geom
         FROM deter.tmp_data
-        WHERE biome='{biome}';
+        WHERE biome='{biome}' AND geocode IS NOT NULL
     """
     logger.debug(sql)
     deter = gpd.GeoDataFrame.from_postgis(sql=sql, con=db.conn, geom_col="geom")
